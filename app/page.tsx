158fc07d8fdd7b8428f93ef526ad7b8ecd81e056
aetherxt@archlinux <adatanium@gmail.com>
2026-02-15 19:43:37 +0800

    changed navbar to horizontal and added responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 import Navbar from '@/components/navbar';
 
-export const metadata = {
-  title: 'MLAW',
-};
-
 export default function Home() {
   return (
     <div>
-      <div className="flex min-h-screen bg-slate-50 font-mono dark:bg-slate-900">
+      <div className="flex flex-col min-h-screen bg-stone-50 font-[Geist_Mono] dark:bg-stone-900 selection:bg-indigo-100 selection:text-indigo-900">
         <Navbar />
-        <div className="flex flex-col justify-start gap-4 pl-15 pt-20">
-          <div className="text-6xl font-bold">Marcus Law</div>
-          <div className="text-2xl opacity-70 ml-5">BEng(Compsci) @ HKU</div>
+        <div className="flex flex-col justify-start gap-4 px-15">
+          <div className="text-4xl sm:text-6xl font-medium">Marcus Law</div>
+          <div className="text-lg sm:text-2xl text-slate-500 dark:text-slate-400 ml-5">BEng(Compsci) @ HKU</div>
+        </div>
+        <div className="flex flex-col px-10 py-50">
+          <div className="text-9xl pt-50">test</div>
+          <div className="text-9xl pt-50">test</div>
+          <div className="text-9xl pt-50">test</div>
         </div>
       </div>
     </div>
