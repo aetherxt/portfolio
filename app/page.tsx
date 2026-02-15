4087d182f7ed3f3d7877d193fc739cebdcb4026e
aetherxt <77727224+aetherxt@users.noreply.github.com>
2026-02-15 20:42:11 +0800

    added navbar gradient and fixed overflow discolor

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,18 @@ import Navbar from '@/components/navbar';
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-col min-h-screen bg-stone-50 font-[Geist_Mono] dark:bg-stone-900 selection:bg-indigo-100 selection:text-indigo-900">
+      <div className="flex flex-col h-full bg-stone-50 font-[Geist_Mono] dark:bg-stone-900 selection:bg-indigo-100 selection:text-indigo-900">
         <Navbar />
-        <div className="flex flex-col justify-start gap-4 px-15">
-          <div className="text-4xl sm:text-6xl font-medium">Marcus Law</div>
-          <div className="text-lg sm:text-2xl text-slate-500 dark:text-slate-400 ml-5">BEng(Compsci) @ HKU</div>
-        </div>
-        <div className="flex flex-col px-10 py-50">
-          <div className="text-9xl pt-50">test</div>
-          <div className="text-9xl pt-50">test</div>
-          <div className="text-9xl pt-50">test</div>
+        <div className="pl-15 sm:pl-30">
+          <div className="flex flex-col justify-start gap-4">
+            <div className="text-4xl sm:text-6xl font-medium">Marcus Law</div>
+            <div className="text-lg sm:text-2xl text-slate-500 dark:text-slate-400 ml-5">BEng(Compsci) @ HKU</div>
+          </div>
+          <div className="flex flex-col px-10 py-50">
+            <div className="text-9xl pt-50">test</div>
+            <div className="text-9xl pt-50">test</div>
+            <div className="text-9xl pt-50">test</div>
+          </div>
         </div>
       </div>
     </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,8 +2,8 @@
 
 export default function Navbar() {
     return (
-        <div className="flex max-h-1/10 max-w-screen place-self-start mb-15 sticky top-0 z-10">
-            <div className="flex flex-col sm:flex-row sm:items-center mt-10 mx-10 sm:mx-25 select-none gap-3 sm:gap-5">
+        <div className="flex w-full place-self-start sticky top-0 z-10 bg-gradient-to-b from-stone-50/95 via-stone-50/60 to-stone-50/0 dark:from-stone-900/95 dark:via-stone-900/60 dark:to-stone-900/0 backdrop-blur-sm">
+            <div className="flex flex-col sm:flex-row sm:items-center my-5 sm:my-15 mx-10 sm:mx-20 select-none gap-3 sm:gap-5">
                 <a
                     href="#"
                     className="text-lg hover:bg-indigo-100 hover:text-indigo-900 mr-8"
