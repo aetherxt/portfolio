2389a6fdec47f5eec0c09c12b63d008ea4daddf2
aetherxt@archlinux <adatanium@gmail.com>
2026-02-23 21:17:11 +0800

    photo locations

--- app/photography/24-3-2024/page.tsx
+++ app/photography/24-3-2024/page.tsx
@@ -13,9 +13,12 @@ export default function March242024() {
         <div className="flex flex-col h-full bg-stone-50 font-[Geist_Mono] dark:bg-stone-900 selection:bg-indigo-100 selection:text-indigo-900">
             <Navbar />
             <div className="px-15 sm:px-30 pt-5 pb-30">
-                <div className="flex flex-col justify-start gap-2 sm:gap-4">
+                <div className="flex flex-col justify-start">
                     <a href="/photography" className="text-blue-900 dark:text-blue-100 hover:bg-indigo-100 hover:text-indigo-900 ml-3 w-fit">&lt; back</a>
-                    <div className="text-2xl sm:text-3xl font-medium">24-3-2024</div>
+                    <div className="text-2xl sm:text-3xl font-medium pt-2 sm:pt-5">March 24, 2024</div>
+                    <div className="flex flex-row items-center pl-5 pt-2">
+                        <span className="text-stone-800/75 dark:text-stone-200/75">Kowloon Bay | Hong Kong</span>
+                    </div>
                 </div>
                 <ImageGallery images={imageFiles} basePath="/photography/24-3-2024" />
             </div>
--- app/photography/awaji26/page.tsx
+++ app/photography/awaji26/page.tsx
@@ -12,10 +12,13 @@ export default function Awaji26() {
     return (
         <div className="flex flex-col h-full bg-stone-50 font-[Geist_Mono] dark:bg-stone-900 selection:bg-indigo-100 selection:text-indigo-900">
             <Navbar />
-            <div className="px-15 sm:px-30 pt-5 pb-30">
-                <div className="flex flex-col justify-start gap-2 sm:gap-4">
+            <div className="px-15 sm:px-30 sm:pt-5 pb-30">
+                <div className="flex flex-col justify-start">
                     <a href="/photography" className="text-blue-900 dark:text-blue-100 hover:bg-indigo-100 hover:text-indigo-900 ml-3 w-fit">&lt; back</a>
-                    <div className="text-2xl sm:text-3xl font-medium">Awaji 2026</div>
+                    <div className="text-2xl sm:text-3xl font-medium pt-2 sm:pt-5">Awaji 2026</div>
+                    <div className="flex flex-row items-center pl-5 pt-2">
+                        <span className="text-stone-800/75 dark:text-stone-200/75">Awaji; Tokushima; Kobe; Osaka | Japan</span>
+                    </div>
                 </div>
                 <ImageGallery images={imageFiles} basePath="/photography/Awaji26" />
             </div>
