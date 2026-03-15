f0ccdf6f0a98805c574fdbdd565b9b8a4bcead79
aetherxt <77727224+aetherxt@users.noreply.github.com>
2026-03-15 14:46:24 +0800

    typo

--- app/photography/11-3-2026/page.tsx
+++ app/photography/11-3-2026/page.tsx
@@ -15,7 +15,7 @@ export default function April122024() {
             <div className="px-15 sm:px-30 pt-5 pb-30">
                 <div className="flex flex-col justify-start">
                     <a href="/photography" className="text-blue-900 dark:text-blue-100 hover:bg-indigo-100 hover:text-indigo-900 ml-3 w-fit">&lt; back</a>
-                    <div className="text-2xl sm:text-3xl font-medium pt-2 sm:pt-5">11 March, 2026</div>
+                    <div className="text-2xl sm:text-3xl font-medium pt-2 sm:pt-5">March 11, 2026</div>
                     <div className="flex flex-row items-center pl-5 pt-2">
                         <span className="text-stone-800/75 dark:text-stone-200/75">Innocell | Hong Kong; Qianhai | Shenzhen</span>
                     </div>
